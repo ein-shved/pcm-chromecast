75bed99794dacdb357d643ce79c8c428b8239b3a
Yury Shvedov <yury.shvedov@kaspersky.com>
2024-01-28 20:15:45 +0300

    Nix development shell
    Add nix devShell with all needed python packages and development
    helpers.
    
    Change-Id: I44e1978c871784f17d58ba5d5347b2f72945def7

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        pypackages = ps: with ps; [
+          zeroconf
+          pychromecast
+          flask
+          pyaudio
+          ffmpeg-python
+          uritools
+          uuid
+          pylint
+        ];
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell {
+            packages = [
+              (pkgs.python3.withPackages pypackages)
+              pkgs.ffmpeg
+              pkgs.pyright
+            ];
+          };
+        };
+      });
+}
